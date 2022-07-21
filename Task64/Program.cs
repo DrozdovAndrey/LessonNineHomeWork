b39814068238eb4bb62747041fad034f22aa4bdb
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-21 23:13:18 +0300

    task64 done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,3 +3,38 @@
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 
 // M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+
+
+namespace HomeWork9
+{
+    class Task64
+    {
+        static void Main(string[] args)
+        {
+            int n = new Random().Next(3,10);
+            int m = new Random().Next(3,10);
+            Console.WriteLine($"n = {n},  m = {m}");
+            if (m == n) Console.WriteLine(n);
+            if (n < m) 
+            {
+                while(n < m)
+                {
+                    Console.Write(n);
+                    n++;
+                }
+                Console.Write(m);
+            }
+            if (n > m)
+            {
+                while(n > m)
+                {
+                    Console.Write(n);
+                    n--;
+                }
+                Console.Write(m);
+            }
+        }
+    }
+}

226a0b74809f4726feb5f11ec20f5de6ade9d9cd
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-21 23:30:43 +0300

    task66 added

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -2,3 +2,35 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+namespace HomeWork9
+{
+    class Task66
+    {
+        static void Main(string[] args)
+        {
+            int n = new Random().Next(3,10);
+            int m = new Random().Next(3,10);
+            Console.WriteLine($"n = {n},  m = {m}");
+            int sum = 0;
+            if (m == n) Console.WriteLine(n);
+            if (n < m) 
+            {  
+                for (int i = n; i <= m; i++)
+                {
+                    sum += i;
+                    
+                }
+                Console.Write(sum);            
+            }
+            if (n > m)
+            {
+                for (int j = n; j >= m; j--)
+                {
+                    sum += j;
+                }
+                Console.Write(sum);
+            }
+        }
+    }
+}
